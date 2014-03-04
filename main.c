fe6e77802a7bfeaa8eb8066a4d60a92269e14982
Christoph Recht <christoph@littleblackdeath.home.de>
2014-03-04 21:47:48 +0100

    first blinking LED

--- main.c
+++ main.c
@@ -4,11 +4,24 @@
  *  Created on: 04.03.2014
  *      Author: christoph
  */
+#include <avr/io.h>
+#include <avr/delay.h>
 
+void wait_ms(unsigned int time)
+{
+	while(time--)
+		_delay_ms(1);
+}
 
 
 int main()
 {
-	while(1);
+	// GREEN LED config on PB2
+	DDRB |= ( 1 << PB2);
+	while(1)
+	{
+		wait_ms(1000);
+		PORTB ^= (1 << PB2);
+	}
 	return 0;
 }
